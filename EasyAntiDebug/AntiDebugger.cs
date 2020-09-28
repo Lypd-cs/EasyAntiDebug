4cea194575e32707031240c634811bbcce60d82a
Lypd-cs <67859287+Lypd-cs@users.noreply.github.com>
2020-09-28 20:16:22 +0200

    Add files via upload

---
+++ EasyAntiDebug/AntiDebugger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace EasyAntiDebug
+{
+   
+    public class AntiDebugger
+    {
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        private static extern bool CheckRemoteDebuggerPresent(IntPtr ProcessHandle, ref bool isDebugging);
+
+        public static bool isDebugging()
+        {
+            bool isDebugging = false;
+            CheckRemoteDebuggerPresent(Process.GetCurrentProcess().Handle, ref isDebugging);
+
+            if (isDebugging)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
